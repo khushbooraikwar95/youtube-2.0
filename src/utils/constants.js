3644093af4693966cf0fd9b370f25310d9780003
khushbooraikwar95 <j.khushbooraikwar95@gmail.com>
2023-05-23 20:53:26 +0530

    feat - Youtube API call on VedioContainer

---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+const GOOGLE_API_KEY = "AIzaSyBCY55lfEN5pNkRFa2gIlTJxIYM6K4-DCA";
+
+export const YOUTUBE_VIDEOS_API =
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=" +
+  GOOGLE_API_KEY;
